f24dd6577855ac0d2d1b048b41212e540e6b18af
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-09 01:10:31 +0900

    feat(validateEnclosedInBrackets): 상품-수량이 대괄호로 ë¬묶여있는지 검증하는 ㄴ기능 구현

--- src/main/java/store/view/ProductSelectionInputHandler.java
+++ src/main/java/store/view/ProductSelectionInputHandler.java
@@ -2,12 +2,34 @@ package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ProductSelectionInputHandler {
 
     public static String promptProductSelection() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1]");;
-        String productNameAndQuantity =  Console.readLine();
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1]");
 
-        return productNameAndQuantity;
+        String input = Console.readLine();
+        validateProductSelection(input);
+        return input;
+    }
+
+    public static Map<String, Integer> validateProductSelection(String input) {
+        List<String> withoutBrackets = validateEnclosedInBrackets(input);
+        return null;
+    }
+
+    private static List<String> validateEnclosedInBrackets(String input) {
+        String[] nameAndQuantityPair = input.split(",");
+        List<String> result = new ArrayList<>();
+        for (String nameAndQuantity : nameAndQuantityPair) {
+            if (!nameAndQuantity.startsWith("[") || !nameAndQuantity.endsWith("]")) {
+                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+            }
+            result.add(nameAndQuantity.substring(1, nameAndQuantity.length() - 1));
+        }
+        return result;
     }
 }
--- src/test/java/store/view/ProductSelectionInputHandlerTest.java
+++ src/test/java/store/view/ProductSelectionInputHandlerTest.java
@@ -1,20 +1,19 @@
 package store.view;
 
-import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.Test;
-import java.io.ByteArrayInputStream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ProductSelectionInputHandlerTest {
-    @Test
-    void 입력_받은_개별_상품들은_대괄호로_묶여있지_않으면_예외_발생() {
-        String input = "[콜라-10],[사이다-3";
-
-        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        assertThatThrownBy(() -> ProductSelectionInputHandler.promptProductSelection())
+    @ParameterizedTest
+    @ValueSource(strings = {"[콜라-10],[사이다-3", "콜라-10],[사이다-3]", "[콜라10,사이다3]"})
+    void 입력_받은_개별_상품들은_대괄호로_묶여있지_않으면_예외_발생(String input) {
+        assertThatThrownBy(() -> ProductSelectionInputHandler.validateProductSelection(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
     }
