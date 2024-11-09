f4d20e51cd986ee0b1f4e2986a2fdb663b6e4871
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 03:24:50 +0900

    feat(validateInput): 멤버십 할인 여부 입력ì을 검증하는 기능 구현

--- src/main/java/store/view/MembershipDiscountInputHandler.java
+++ src/main/java/store/view/MembershipDiscountInputHandler.java
@@ -1,5 +1,23 @@
 package store.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class MembershipDiscountInputHandler {
-    public static prompt
+
+
+    public static Boolean promptMembershipDiscount() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        String input = Console.readLine();
+        return validateInput(input);
+    }
+
+    public static Boolean validateInput(String input) {
+        if (!(input == "Y" || input == "N")) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
+        if (input == "Y") {
+            return true;
+        }
+        return false;
+    }
 }
