dc5237f501dce0cfa622066b4a12f72f86a61068
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 02:05:53 +0900

    test(promotionApplicableQuantity): 프로모션 적용 ê°된 결제가 가능한 수량을 반환하는지 테스트

--- src/test/java/store/domain/PurchaseItemTest.java
+++ src/test/java/store/domain/PurchaseItemTest.java
@@ -9,6 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PurchaseItemTest {
 
     @Test
+    void 프로모션_적용_가능한_수량을_반환() {
+        // given
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, null, null);
+        Product product = new Product("콜라", 1000, 10, 10, promotion);
+        PurchaseItem purchaseItem = new PurchaseItem(product, 5);
+        int expected = product.getPromoQuantity() / (promotion.getBuy() + promotion.getGet());
+
+        // when
+        int result = purchaseItem.promotionApplicableQuantity();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     void 추가_가능한_프로모션_수량이_있는지_반환한다() {
         // given
         LocalDate start = LocalDate.of(2024, 01, 01);
@@ -24,4 +39,21 @@ public class PurchaseItemTest {
         // then
         assertThat(result).isTrue();
     }
+
+//    @Test
+//    void 프로모션_재고가_입력_수량보다_적은지_반환한다() {
+//        // given
+//        LocalDate start = LocalDate.of(2024, 01, 01);
+//        LocalDate end = LocalDate.of(2024, 12, 31);
+//        Promotion promotion = new Promotion("탄산2+1", 2, 1, start, end);
+//        Product product = new Product("콜라", 1000, 10, 5, promotion);
+//        int quantity = 10;
+//        PurchaseItem purchaseItem = new PurchaseItem(product, quantity);
+//
+//        // when
+//        Boolean result = purchaseItem.isPromotionStockInsufficient();
+//
+//        // then
+//        assertThat(result).isTrue();
+//    }
 }
