7e2d11c37a2f30d4345089c9db30a3668983e610
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 13:58:09 +0900

    test(nubmerOfFree): 증정 수량을 올바르게 반환하는지 테스트

--- src/test/java/store/domain/PurchaseItemTest.java
+++ src/test/java/store/domain/PurchaseItemTest.java
@@ -26,9 +26,7 @@ public class PurchaseItemTest {
     @Test
     void 추가_가능한_프로모션_수량이_있는지_반환한다() {
         // given
-        LocalDate start = LocalDate.of(2024, 01, 01);
-        LocalDate end = LocalDate.of(2024, 12, 31);
-        Promotion promotion = new Promotion("탄산2+1", 2, 1, start, end);
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, null, null);
         Product product = new Product("콜라", 1000, 10, 10, promotion);
         int quantity = 5;
         PurchaseItem purchaseItem = new PurchaseItem(product, quantity);
@@ -38,5 +36,35 @@ public class PurchaseItemTest {
 
         // then
         assertThat(result).isTrue();
+    }
+
+    @Test
+    void 증정_수량_반환_프로모션_재고가_충분한_경우() {
+        // given
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, null, null);
+        Product product = new Product("콜라", 1000, 10, 10, promotion);
+        PurchaseItem purchaseItem = new PurchaseItem(product, 10);
+        int expected = 3;
+
+        // when
+        int result = purchaseItem.numberOfFree();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 증정_수량_반환_프로모션_재고가_부족한_경우() {
+        // given
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, null, null);
+        Product product = new Product("콜라", 1000, 10, 10, promotion);
+        PurchaseItem purchaseItem = new PurchaseItem(product, 13);
+        int expected = 3;
+
+        // when
+        int result = purchaseItem.numberOfFree();
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
--- src/test/java/store/view/AddFreeInputHandlerTest.java
+++ src/test/java/store/view/AddFreeInputHandlerTest.java
@@ -1,18 +1,26 @@
 package store.view;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class AddFreeInputHandlerTest {
-    @Test
-    void 입력이_올바르지_않을_경우_예외_발생() {
-        // given
-        String input = "1";
-
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "1", ","})
+    void 입력이_올바르지_않을_경우_예외_발생(String input) {
         // when & then
         assertThatThrownBy(() -> AddFreeInputHandler.validateInput(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Y", "N"})
+    void 입력이_올바르면_예외_없음(String input) {
+        // when & then
+        assertDoesNotThrow(() -> PayRegularPriceInputHandler.validateInput(input));
     }
 }
