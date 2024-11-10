8eb744004e2709a27e6fcb4993b74cd1bd6e3137
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 16:19:57 +0900

    test(createReceipt): 영수증 객체 생성이 올바른지 테스트

--- src/test/java/store/service/PurchaseServiceTest.java
+++ src/test/java/store/service/PurchaseServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import store.domain.Product;
 import store.domain.Promotion;
 import store.domain.PurchaseItem;
+import store.domain.Receipt;
 
 import java.util.*;
 
@@ -35,26 +36,25 @@ public class PurchaseServiceTest {
         assertThat(result.size()).isEqualTo(expected.size());
     }
 
-//    @Test
-//    void 영수증_객체_생성이_올바른지_테스트() {
-//        // given
-//        Promotion promotion = null;
-//        Product product1 = new Product("콜라", 10, 10, 10, promotion);
-//        Product product2 = new Product("사이다", 10, 10, 10, promotion);
-//        PurchaseItem purchaseItem1 = new PurchaseItem(product1, 10);
-//        PurchaseItem purchaseItem2 = new PurchaseItem(product2, 10);
-//        List<PurchaseItem> expected = new ArrayList<>(List.of(purchaseItem1, purchaseItem2));
-//
-//        Map<Product, Integer> productandQuantityPairs = new LinkedHashMap<>();
-//        productandQuantityPairs.put(product1, 10);
-//        productandQuantityPairs.put(product2, 10);
-//
-//        // when
-//        List<PurchaseItem> result = purchaseService.createPurchaseItems(productandQuantityPairs);
-//
-//        // then
-//        assertThat(result.get(0).getProduct().getName()).isEqualTo(product1.getName());
-//        assertThat(result.get(1).getProduct().getName()).isEqualTo(product2.getName());
-//        assertThat(result.size()).isEqualTo(expected.size());
-//    }
+    @Test
+    void 영수증_객체_생성이_올바른지_테스트() {
+        // given
+        Promotion promotion = null;
+        Product product1 = new Product("콜라", 10, 10, 10, promotion);
+        Product product2 = new Product("사이다", 10, 10, 10, promotion);
+        PurchaseItem purchaseItem1 = new PurchaseItem(product1, 10);
+        PurchaseItem purchaseItem2 = new PurchaseItem(product2, 10);
+        List<PurchaseItem> purchaseItems = new ArrayList<>(List.of(purchaseItem1, purchaseItem2));
+        String expected1 = "콜라";
+        String expected2 = "사이다";
+
+        // when
+        Receipt receipt = purchaseService.createReceipt(purchaseItems);
+        String result1 = receipt.getPurchaseItems().get(0).getProduct().getName();
+        String result2 = receipt.getPurchaseItems().get(1).getProduct().getName();
+
+        // then
+        assertThat(result1).isEqualTo(expected1);
+        assertThat(result2).isEqualTo(expected2);
+    }
 }
