1f19541126e98cbe69e0d39f1040ee53a1a9a126
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 14:40:30 +0900

    test(totalPurchaseAmount): 총 구매액을 올바르게 계산하는지 테스트

--- src/test/java/store/domain/ReceiptTest.java
+++ src/test/java/store/domain/ReceiptTest.java
@@ -10,23 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReceiptTest {
 
-//    @Test
-//    void 추가_증정_받겠다고_입력받은_상품의_수량이_증가됐는지_테스트() {
-//        // given
-//        LocalDate start = LocalDate.of(2024, 01, 01);
-//        LocalDate end = LocalDate.of(2024, 12, 31);
-//        Promotion promotion = new Promotion("탄산2+1", 2, 1, start, end);
-//        int quantity = 5;
-//        Product product = new Product("콜라", 1000, 10, 10, promotion);
-//        List<PurchaseItem> purchaseItems = new ArrayList<>(List.of(new PurchaseItem(product, quantity)));
-//        Receipt receipt = new Receipt(purchaseItems);
-//        int expected = quantity + 1;
-//
-//        // when
-//        receipt.addAllFree();
-//        int result = purchaseItems.get(0).getQuantity();
-//
-//        // then
-//        assertThat(result)
-//    }
+    @Test
+    void 총_구매액이_올바른지_테스트() {
+        // given
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, null, null);
+        Product product = new Product("콜라", 1000, 10, 10, promotion);
+        List<PurchaseItem> purchaseItems = new ArrayList<>(List.of(new PurchaseItem(product, 5)));
+        Receipt receipt = new Receipt(purchaseItems);
+        int expected = product.getPrice() * 5;
+
+        // when
+        int result = receipt.totalPurchaseAmount();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
 }
