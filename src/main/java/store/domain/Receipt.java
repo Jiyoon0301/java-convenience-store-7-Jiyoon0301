62520f20fd114b9c931c9d47c5b030fc84134a83
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 14:42:51 +0900

    feat(totalPurchaseAmount): 총 구매액을 반환하는 기능 ê구현

---
+++ src/main/java/store/domain/Receipt.java
@@ -0,0 +1,24 @@
+package store.domain;
+
+import java.util.List;
+
+public class Receipt {
+    private List<PurchaseItem> purchaseItems;
+
+    public Receipt(List<PurchaseItem> purchaseItems) {
+        this.purchaseItems = purchaseItems;
+    }
+
+    public List<PurchaseItem> getPurchaseItems() {
+        return purchaseItems;
+    }
+
+
+    public int totalPurchaseAmount() {
+        int sum = 0;
+        for (PurchaseItem item : purchaseItems) {
+            sum += item.getQuantity() * item.getProduct().getPrice();
+        }
+        return sum;
+    }
+}
