34722974d51292666ded19278c8c98459be3971c
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-10 11:39:29 +0900

    test(AddFreeInputHandler.validateInput): 추가로  증정 여부 입력 검증 테스트

--- src/test/java/store/view/AddFreeInputHandlerTest.java
+++ src/test/java/store/view/AddFreeInputHandlerTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class MembershipDiscountInputHandlerService {
+public class AddFreeInputHandlerTest {
     @Test
     void 입력이_올바르지_않을_경우_예외_발생() {
         // given
-        String input = "a";
+        String input = "1";
 
         // when & then
-        assertThatThrownBy(() -> MembershipDiscountInputHandler.validateInput(input))
+        assertThatThrownBy(() -> AddFreeInputHandler.validateInput(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
     }
