8dee00a4b1e2ee9ffdf043c69d2fc079de1c1d55
ParkJiYoon <89255324+Jiyoon0301@users.noreply.github.com>
2024-11-11 22:57:52 +0900

    refactor(removeStock): 메서드 분리

--- src/main/java/store/service/PurchaseService.java
+++ src/main/java/store/service/PurchaseService.java
@@ -23,15 +23,26 @@ public class PurchaseService {
     public void removeStock(Receipt receipt) {
         for (PurchaseItem item : receipt.getPurchaseItems()) {
             int numberToRemove = item.getQuantity();
-            if (item.getProduct().getPromoStock() != -1 && item.getProduct().getPromoStock() >= numberToRemove) {
-                item.getProduct().removePromoStock(numberToRemove);
-                numberToRemove = 0;
-            }
-            if (item.getProduct().getPromoStock() != -1 && item.getProduct().getPromoStock() < numberToRemove) {
-                numberToRemove -= item.getProduct().getPromoStock();
-                item.getProduct().removePromoStock(item.getProduct().getPromoStock());
-            }
+            numberToRemove = removePromoStockIfMoreStockThanQuantity(item, numberToRemove);
+            numberToRemove = removePromoStockIfMoreQuantityThanStock(item, numberToRemove);
             item.getProduct().removeRegularStock(numberToRemove);
         }
     }
+
+    public int removePromoStockIfMoreStockThanQuantity(PurchaseItem item, int numberToRemove) {
+        if (item.getProduct().getPromoStock() != -1 && item.getProduct().getPromoStock() >= numberToRemove) {
+            item.getProduct().removePromoStock(numberToRemove);
+            return 0;
+        }
+        return numberToRemove;
+    }
+
+    public int removePromoStockIfMoreQuantityThanStock(PurchaseItem item, int numberToRemove) {
+        if (item.getProduct().getPromoStock() != -1 && item.getProduct().getPromoStock() < numberToRemove) {
+            numberToRemove -= item.getProduct().getPromoStock();
+            item.getProduct().removePromoStock(item.getProduct().getPromoStock());
+        }
+        return numberToRemove;
+    }
+
 }
